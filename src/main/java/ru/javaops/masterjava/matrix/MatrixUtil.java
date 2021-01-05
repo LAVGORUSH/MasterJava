b308b09c25279e3187b2cecb3017b8f7ef0d97e2
Lebedev Anton <lavgorush@gmail.com>
2021-01-05 12:25:57 +0300

    HW01 add matrix optimasation and realise concurrentMultiply

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
 
 /**
  * gkislin
@@ -10,12 +11,35 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
+    private static final ExecutorService multiplyExecutor = Executors.newFixedThreadPool(MainMatrix.THREAD_NUMBER);
+
     // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        final CompletionService<Void> completionService = new ExecutorCompletionService<>(multiplyExecutor);
+        List<Future<Void>> futures = new ArrayList<>();
+
+        for (int i = 0; i < matrixSize; i++) {
+            int index = i;
+            futures.add(completionService.submit(() -> multiplyMatrix(matrixA[index], matrixB, matrixC, index, matrixSize)));
+        }
+        while (!futures.isEmpty()) {
+            futures.remove(completionService.poll());
+        }
         return matrixC;
+    }
+
+    private static Void multiplyMatrix(int[] row, int[][] matrixB, int[][] matrixC, int i, int matrixSize) {
+        int[] column = new int[matrixSize];
+        for (int k = 0; k < matrixSize; k++) {
+            column[k] = matrixB[k][i];
+        }
+        for (int j = 0; j < matrixSize; j++) {
+            matrixC[i][j] = getSum(column, row);
+        }
+        return null;
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
@@ -24,15 +48,18 @@ public class MatrixUtil {
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
         for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
-                }
-                matrixC[i][j] = sum;
-            }
+            multiplyMatrix(matrixA[i], matrixB, matrixC, i, matrixSize);
         }
         return matrixC;
+    }
+
+    private static int getSum(int[] column, int[] row) {
+        final int matrixSize = column.length;
+        int sum = 0;
+        for (int k = 0; k < matrixSize; k++) {
+            sum += row[k] * column[k];
+        }
+        return sum;
     }
 
     public static int[][] create(int size) {
